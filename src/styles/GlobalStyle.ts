06ba36a1ceff11ce3eb6a15ca8cd6c4895668ab2
Mauricio R Duarte <mauriciotem@gmail.com>
2020-11-17 12:26:22 -0300

    first commit

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background-color: #f3f3f3;
+    color: #000;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
+  }
+`;
